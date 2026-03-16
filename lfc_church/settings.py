b9236b6b111aae67c3eed87381f6901647f6a485
albinmaniac <www.albinmania@gmail.com>
2026-03-16 17:20:24 +0530

    fix b2 storage

--- lfc_church/settings.py
+++ lfc_church/settings.py
@@ -185,7 +185,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_SSL_REDIRECT = True
 
 
-
+AWS_S3_ADDRESSING_STYLE = "path"
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_ACCESS_KEY_ID = os.environ.get("B2_KEY_ID")
